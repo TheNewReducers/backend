36441dbcecdb648d5cddc1aa24da06e6fd1ff39b
timkn <contatc@knothe.me>
2023-03-23 11:47:59 +0100

    added some other vision functions

--- image_processing.py
+++ image_processing.py
@@ -1,6 +1,33 @@
 from google.cloud import vision
 import io
 
+from typing import Sequence
+
+
+def analyze_image_from_uri(
+    image_uri: str,
+    feature_types: Sequence,
+) -> vision.AnnotateImageResponse:
+    client = vision.ImageAnnotatorClient()
+
+    image = vision.Image()
+    image.source.image_uri = image_uri
+    features = [vision.Feature(type_=feature_type) for feature_type in feature_types]
+    request = vision.AnnotateImageRequest(image=image, features=features)
+
+    response = client.annotate_image(request=request)
+
+    return response
+def print_text(response: vision.AnnotateImageResponse):
+    print("=" * 80)
+    for annotation in response.text_annotations:
+        vertices = [f"({v.x},{v.y})" for v in annotation.bounding_poly.vertices]
+        print(
+            f"{repr(annotation.description):42}",
+            ",".join(vertices),
+            sep=" | ",
+        )
+
 def detect_text(path):
     """Detects text in the file."""
 
@@ -31,5 +58,39 @@ def detect_text(path):
 
 #https://cloud.google.com/vision/docs/detect-labels-image-client-libraries
 
+def detect_labels(path):
+    import io
+    import os
+
+    # Imports the Google Cloud client library
+    from google.cloud import vision
+
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+
+    # The name of the image file to annotate
+    file_name = os.path.abspath(path)
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
 if __name__ == '__main__':
-    detect_text("./test-data/kassenbon-1.jpg")
+    #detect_labels("./test-data/kassenbon-1.jpg")
+    #detect_text("./test-data/kassenbon-1.jpg")
+
+    image_uri = "./test-data/kassenbon-1.jpg"
+    features = [vision.Feature.Type.TEXT_DETECTION]
+
+    response = analyze_image_from_uri(image_uri, features)
+    print_text(response)

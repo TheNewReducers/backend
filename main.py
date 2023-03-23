d0a664a3c1d58d01d6c5f9399016663292bf8f7d
16flotho <tom.florian@hotmail.com>
2023-03-23 14:34:06 +0100

    feat: add gunicorn config

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile):
     return {"filename": file.filename}

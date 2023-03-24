f05790d63929d08a81edd1d2f9418672021c8df2
Tim Knothe <contatc@knothe.me>
2023-03-24 04:41:24 +0100

    beta v3

--- main.py
+++ main.py
@@ -1,32 +1,16 @@
-from fastapi import FastAPI, UploadFile
-from typing import Annotated
-
 from fastapi import FastAPI, File, UploadFile
-
 import controller
-import image_processing
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile):
     file_process = file.file
-
     resp: dict = controller.file_input(file_process)
-    print(type(resp))
     return resp
-
-@app.get("/test/")
-async def test():
-   return str(image_processing.test())
 
 
 @app.get("/categories")
 async def return_all_categories():
-    return None
+    return ["Meat", "Pet Food", "Snacks", "Fruits", "Vegetables", "Dairy", "Beverages", "Tobacco", "Alcohol"]
